a5a88eb0587fa268b765269c7077678fe2921f4d
Vighnesh Tathare <v42069@yahoo.com>
2025-02-28 21:54:22 +0530

    add

---
+++ src/FunctionalProgrammingTechniques/TailCallRecursion.java
@@ -0,0 +1,22 @@
+package FunctionalProgrammingTechniques;
+
+public class TailCallRecursion {
+	
+	public static int fact(int n) {
+		if(n<=1)return 1;
+		return n*fact(n-1);
+	}
+	
+	public static int tailRecursionFact(int n ,int a) {
+		if(n<=1) {
+			return a;
+		}
+		return tailRecursionFact(n-1, n*a);
+	}
+
+	
+	public static void main(String[] args) {
+		System.out.println(fact(4));
+		System.out.println(tailRecursionFact(4, 1));
+	}
+}

6549c1654030e3c5b93a59bd56ab3ad4a2d281cd
Vighnesh Tathare <v42069@yahoo.com>
2025-08-20 23:04:55 +0530

    add

--- src/FunctionalProgrammingTechniques/FunctionComposition/FunctionConposition.java
+++ src/FunctionalProgrammingTechniques/FunctionComposition/FunctionConposition.java
@@ -6,24 +6,25 @@ public class FunctionConposition {
 
 		// using inbuilt function
 
-		// Function<Integer, Integer> f1=s->s+5;
-		// Function<Integer, Integer> f2=s->s*5;
+		 Function<Integer, Integer> f1=s->s+5;
+		 Function<Integer, Integer> f2=s->s*5;
 
-		// Function<Integer, Integer> f3=f1.compose(f2);//right to left
-		// System.out.println(f3.apply(5));
+		 Function<Integer, Integer> f3=f1.compose(f2);//right to left
+		 System.out.println(f3.apply(5));
+//		 System.out.println(f3.apply(5));
 
 		// Custom function compositoon
-		Function<Square, Integer> f1 = s -> s.getArea();
-		Function<Integer, Double> f2 = s -> Math.sqrt(s);
-
-		Function<Square, Double> getSide = f2.compose(f1);
-
-		Square s = new Square();
-		s.setArea(100);
-
-		Double side = getSide.apply(s);
-
-		System.out.println(side);
+//		Function<Square, Integer> f1 = s -> s.getArea();
+//		Function<Integer, Double> f2 = s -> Math.sqrt(s);
+//
+//		Function<Square, Double> getSide = f2.compose(f1);
+//
+//		Square s = new Square();
+//		s.setArea(100);
+//
+//		Double side = getSide.apply(s);
+//
+//		System.out.println(side);
 
 	}
 }
